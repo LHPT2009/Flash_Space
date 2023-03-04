fa89c3f0cf14e566ea11bba02e1368526e71f7e6
LeHuynhPhuongTung <lehuynhphuongtung0601@gmail.com>
2023-03-05 02:07:38 +0700

    feature/website

--- Website/src/Screens/Auth/Reset/Confirm.jsx
+++ Website/src/Screens/Auth/Reset/Confirm.jsx
@@ -1,73 +1,85 @@
 import "./Confirm.css";
 import { Link } from "react-router-dom";
+import TopNav from "../../../components/TopNav/TopNav";
+import Footer from "../../../components/Footer/Footer";
 
 const Confirm = () => {
   return (
-    <div className="bodylogin">
-      <div class="container">
-        <div class="cardlogin">
-          <div class="row g-0">
-            <div class="col-md-6">
-              <div
-                class="d-flex justify-content-center align-items-center"
-                style={{ height: "450px" }}
-              >
-                <div class="py-4 px-3 w-75">
-                  <h4>Confirm</h4>
-                  <div class="row mt-2">
-                    <div class="col-md-12">
-                      <div class="input-field">
-                        <input class="form-control" id="input3" required />
-                        <label for="input3">Passcode</label>
+    <>
+      <TopNav />
+      <div
+        className="bodyconfirm"
+        style={{
+          backgroundImage:
+            "url(https://images.pexels.com/photos/1457842/pexels-photo-1457842.jpeg?auto=compress&cs=tinysrgb&w=1600)",
+        }}
+      >
+        <div class="container">
+          <div class="cardlogin">
+            <div class="row g-0">
+              <div class="col-md-6">
+                <div
+                  class="d-flex justify-content-center align-items-center"
+                  style={{ height: "450px" }}
+                >
+                  <div class="py-4 px-3 w-75">
+                    <h4>Confirm</h4>
+                    <div class="row mt-2">
+                      <div class="col-md-12">
+                        <div class="input-field">
+                          <input class="form-control" id="input3" required />
+                          <label for="input3">Passcode</label>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div class="row mt-2">
-                    <div class="col-md-12">
-                      <button class="btn btn-primary w-100 signup-button">
-                        next
-                      </button>
+                    <div class="row mt-2">
+                      <div class="col-md-12">
+                        <button class="btn btn-primary w-100 signup-button">
+                          next
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div class="col-md-6">
-              <div class="right-side-content">
-                <div class="content d-flex flex-column">
-                  <h6>Explore you activity</h6>
-                  <span>
-                    sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua
-                  </span>
-                </div>
-                <div class="right-side">
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span>
-                    <img src="https://i.imgur.com/kWmyZvb.jpg" />
-                  </span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span>
-                    <img src="https://i.imgur.com/U0863iD.jpg" />
-                  </span>
-                  <span></span>
+              <div class="col-md-6">
+                <div class="right-side-content-confirm">
+                  <div class="content d-flex flex-column">
+                    <h6>Explore you activity</h6>
+                    <span>
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua
+                    </span>
+                  </div>
+                  <div class="right-side">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span>
+                      <img src="https://i.imgur.com/kWmyZvb.jpg" />
+                    </span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span>
+                      <img src="https://i.imgur.com/U0863iD.jpg" />
+                    </span>
+                    <span></span>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="parallelogram">
-          <span></span>
-          <span></span>
-          <span></span>
+          <div class="parallelogram">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
--- Website/src/Screens/Auth/Reset/NewReset.jsx
+++ Website/src/Screens/Auth/Reset/NewReset.jsx
@@ -1,81 +1,93 @@
 import "./NewReset.css";
 import { Link } from "react-router-dom";
+import TopNav from "../../../components/TopNav/TopNav";
+import Footer from "../../../components/Footer/Footer";
 
 const NewReset = () => {
   return (
-    <div className="bodylogin">
-      <div class="container">
-        <div class="cardlogin">
-          <div class="row g-0">
-            <div class="col-md-6">
-              <div
-                class="d-flex justify-content-center align-items-center"
-                style={{ height: "450px" }}
-              >
-                <div class="py-4 px-3 w-75">
-                  <h4>New Reset</h4>
-                  <div class="row mt-2">
-                    <div class="col-md-12">
-                      <div class="input-field">
-                        <input class="form-control" id="input3" required />
-                        <label for="input3">Password</label>
+    <>
+      <TopNav />
+      <div
+        className="bodynewreset"
+        style={{
+          backgroundImage:
+            "url(https://images.pexels.com/photos/1457842/pexels-photo-1457842.jpeg?auto=compress&cs=tinysrgb&w=1600)",
+        }}
+      >
+        <div class="container">
+          <div class="cardlogin">
+            <div class="row g-0">
+              <div class="col-md-6">
+                <div
+                  class="d-flex justify-content-center align-items-center"
+                  style={{ height: "450px" }}
+                >
+                  <div class="py-4 px-3 w-75">
+                    <h4>New Reset</h4>
+                    <div class="row mt-2">
+                      <div class="col-md-12">
+                        <div class="input-field">
+                          <input class="form-control" id="input3" required />
+                          <label for="input3">Password</label>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div class="row mt-2 mb-2">
-                    <div class="col-md-12">
-                      <div class="input-field">
-                        <input class="form-control" id="input4" required />
-                        <label for="input4">Password Confirm</label>
+                    <div class="row mt-2 mb-2">
+                      <div class="col-md-12">
+                        <div class="input-field">
+                          <input class="form-control" id="input4" required />
+                          <label for="input4">Password Confirm</label>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div class="row mt-2">
-                    <div class="col-md-12">
-                      <button class="btn btn-primary w-100 signup-button">
-                        NewReset
-                      </button>
+                    <div class="row mt-2">
+                      <div class="col-md-12">
+                        <button class="btn btn-primary w-100 signup-button">
+                          NewReset
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div class="col-md-6">
-              <div class="right-side-content">
-                <div class="content d-flex flex-column">
-                  <h6>Explore you activity</h6>
-                  <span>
-                    sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua
-                  </span>
-                </div>
-                <div class="right-side">
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span>
-                    <img src="https://i.imgur.com/kWmyZvb.jpg" />
-                  </span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span>
-                    <img src="https://i.imgur.com/U0863iD.jpg" />
-                  </span>
-                  <span></span>
+              <div class="col-md-6">
+                <div class="right-side-content-newreset">
+                  <div class="content d-flex flex-column">
+                    <h6>Explore you activity</h6>
+                    <span>
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua
+                    </span>
+                  </div>
+                  <div class="right-side">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span>
+                      <img src="https://i.imgur.com/kWmyZvb.jpg" />
+                    </span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span>
+                      <img src="https://i.imgur.com/U0863iD.jpg" />
+                    </span>
+                    <span></span>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="parallelogram">
-          <span></span>
-          <span></span>
-          <span></span>
+          <div class="parallelogram">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
