a19ce89fee2a090199af23902b0a556ddac81992
LeHuynhPhuongTung <lehuynhphuongtung0601@gmail.com>
2023-03-24 22:16:05 +0700

    upload

--- Website/src/components/TopNav/TopNav.jsx
+++ Website/src/components/TopNav/TopNav.jsx
@@ -23,68 +23,141 @@ const TopNav = () => {
     },
   ];
   return (
-    
-    <nav class="navbar navbar-expand-lg navbar-red navbar-dark">
-      <div class="wrapper"></div>
-      <div class="container-fluid all-show">
-        <a class="navbar-brand" href="#">
-          Penton <i class="fa fa-codepen"></i>
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse " id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
-                About us
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Products
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Services
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Events
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                contact
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <i class="fa fa-search"></i>
-              </a>
-            </li>
-          </ul>
-          <div class="d-flex flex-column sim">
-            <span>1 item added to your quote</span>
-            <small class="text-primary">view your quote</small>
+    <div>
+      <div
+        class="container-fluid text-white d-none d-lg-flex"
+        style={{ backgroundColor: "#004274" }}
+      >
+        <div class="container py-3">
+          <div class="d-flex align-items-center">
+            <a href="index.html" style={{ textDecoration: "none" }}>
+              <h2 class=" fw-bold m-0" style={{ color: "#FFC107" }}>
+                FLASH SPACE
+              </h2>
+            </a>
+            <div class="ms-auto d-flex align-items-center">
+              <small class="ms-4">
+                <i class="fa fa-map-marker-alt me-3"></i>123 Street, New York,
+                USA
+              </small>
+              <small class="ms-4">
+                <i class="fa fa-envelope me-3"></i>info@example.com
+              </small>
+              <small class="ms-4">
+                <i class="fa fa-phone-alt me-3"></i>+012 345 67890
+              </small>
+              <div class="ms-3 d-flex">
+                <a
+                  class="btn btn-sm-square btn-light text-primary rounded-circle ms-2"
+                  href=""
+                >
+                  <i class="fab fa-facebook-f" style={{ color: "#004274" }}></i>
+                </a>
+                <a
+                  class="btn btn-sm-square btn-light text-primary rounded-circle ms-2"
+                  href=""
+                >
+                  <i class="fab fa-twitter" style={{ color: "#004274" }}></i>
+                </a>
+                <a
+                  class="btn btn-sm-square btn-light text-primary rounded-circle ms-2"
+                  href=""
+                >
+                  <i
+                    class="fab fa-linkedin-in"
+                    style={{ color: "#004274" }}
+                  ></i>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </nav>
+
+      <div
+        class="container-fluid sticky-top"
+        style={{
+          backgroundColor: "#004274",
+        }}
+      >
+        <div class="container">
+          <nav class="navbar navbar-expand-lg navbar-light p-lg-0">
+            <a href="index.html" class="navbar-brand d-lg-none">
+              <h1 class="fw-bold m-0" style={{ color: "#FFC107" }}>
+                FLASH SPACE
+              </h1>
+            </a>
+            <button
+              type="button"
+              class="navbar-toggler me-0"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarCollapse"
+            >
+              <span
+                class="navbar-toggler-icon text-white"
+                style={{ color: "white" }}
+              ></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+              <div class="navbar-nav ">
+                <a
+                  href="index.html"
+                  class="nav-item nav-link text-white active"
+                >
+                  Home
+                </a>
+                <a href="about.html" class="nav-item nav-link text-white">
+                  About
+                </a>
+                <a href="service.html" class="nav-item nav-link text-white">
+                  Services
+                </a>
+                <a href="project.html" class="nav-item nav-link text-white">
+                  Projects
+                </a>
+                <div class="nav-item dropdown">
+                  <a
+                    href="#"
+                    class="nav-link dropdown-toggle text-white"
+                    data-bs-toggle="dropdown"
+                  >
+                    Pages
+                  </a>
+                  <div
+                    class="dropdown-menu rounded-0 rounded-bottom m-0"
+                    style={{ backgroundColor: "rgba(0, 66, 116,0.5)" }}
+                  >
+                    <a href="feature.html" class="dropdown-item text-white">
+                      Features
+                    </a>
+                    <a href="team.html" class="dropdown-item text-white">
+                      Our Team
+                    </a>
+                    <a href="testimonial.html" class="dropdown-item text-white">
+                      Testimonial
+                    </a>
+                    <a href="quote.html" class="dropdown-item text-white">
+                      Quotation
+                    </a>
+                    <a href="404.html" class="dropdown-item text-white">
+                      404 Page
+                    </a>
+                  </div>
+                </div>
+                <a href="contact.html" class="nav-item nav-link text-white">
+                  Contact
+                </a>
+              </div>
+              <div class="ms-auto d-none d-lg-block">
+                <a href="" class="btn rounded-pill py-2 px-3 text-white">
+                  Get A Quote
+                </a>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
   );
 };
 
