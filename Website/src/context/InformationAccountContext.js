ff833a0617f6a83883a644f04f8deb52349ab084
LeHuynhPhuongTung <lehuynhphuongtung0601@gmail.com>
2023-04-30 18:31:38 +0700

    update website

--- Website/src/context/InformationAccountContext.js
+++ Website/src/context/InformationAccountContext.js
@@ -2,38 +2,15 @@ import React, { createContext, useState } from "react";
 export const InformationAccountContext = createContext();
 
 const InformationAccountProvider = (props) => {
-  const [informations, setInformations] = useState([]);
-
-  const addInformationAccount = async (information) => {
-    const existedInformationAccount = informations.find((item) => {
-      return item.id === information.id && item.name === information.name;
-    });
-    if (existedInformationAccount) {
-      informations.forEach((item) => {
-        if (item.id === information.id && item.name === information.name) {
-          item.amount += 1;
-        }
-      });
-      setInformations([...informations]);
-    } else {
-      setInformations([...informations, information]);
-    }
-  };
-
-  const delInformationAccount = (name) => {
-    setProducts(products.filter((n) => n.name !== name));
-  };
-
+  const [informations, setInformations] = useState({});
   const clearInformation = () => {
-    setInformations([]);
+    setInformations({});
   };
 
   return (
     <InformationAccountContext.Provider
       value={{
         informations,
-        addInformationAccount,
-        delInformationAccount,
         clearInformation,
       }}
     >
