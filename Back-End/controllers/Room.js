883f099ac2546fc99498ac67ce10c56b6523d663
LeHuynhPhuongTung <lehuynhphuongtung0601@gmail.com>
2023-05-06 18:15:17 +0700

    update be

--- Back-End/controllers/Room.js
+++ Back-End/controllers/Room.js
@@ -2,6 +2,8 @@ const Room = require("../models/Room");
 const WorkAssignment = require("../models/WorkAssignment");
 const WorkingHours = require("../models/WorkingHours");
 const Image = require("../models/Image");
+const path = require("path");
+const sharp = require("sharp");
 
 const RoomController = {
   getAllRoom: async (req, res) => {
@@ -35,91 +37,88 @@ const RoomController = {
 
   addRoom: async (req, res) => {
     try {
-      const arrworkAssignment = await WorkAssignment.aggregate([
-        {
-          $group: {
-            _id: "$idaccount",
-            count: { $sum: 1 },
-          },
-        },
-        {
-          $project: {
-            _id: 0,
-            idaccount: "$_id",
-            amount: "$count",
-          },
-        },
-      ]);
-      const sortarr = arrworkAssignment
-        .sort((a, b) => a.amount - b.amount)
-        .sort();
+      // const arrworkAssignment = await WorkAssignment.aggregate([
+      //   {
+      //     $group: {
+      //       _id: "$idaccount",
+      //       count: { $sum: 1 },
+      //     },
+      //   },
+      //   {
+      //     $project: {
+      //       _id: 0,
+      //       idaccount: "$_id",
+      //       amount: "$count",
+      //     },
+      //   },
+      // ]);
+      // const sortarr = arrworkAssignment.sort((a, b) => a.amount - b.amount);
 
-      const newRoom = await new Room({
-        idward: req.body.idward,
-        idcareer: req.body.idcareer,
-        idaccount: req.body.idaccount,
-        length: req.body.length,
-        width: req.body.width,
-        price: req.body.price,
-        longitude: req.body.longitude,
-        latitude: req.body.latitude,
-        static: 0,
-        subject: req.body.subject,
-        describe: req.body.describe,
-        housenumberstreetname: req.body.housenumberstreetname,
-        mainimage: req.files[0].filename.split(".", 1) + ".jpeg",
-      });
-      await newRoom.save();
+      // const newRoom = await new Room({
+      //   idward: req.body.idward,
+      //   idcareer: req.body.idcareer,
+      //   idaccount: req.body.idaccount,
+      //   length: req.body.length,
+      //   width: req.body.width,
+      //   price: req.body.price,
+      //   longitude: req.body.longitude,
+      //   latitude: req.body.latitude,
+      //   static: 0,
+      //   subject: req.body.subject,
+      //   describe: req.body.describe,
+      //   housenumberstreetname: req.body.housenumberstreetname,
+      //   mainimage: req.files[0].filename.split(".", 1) + ".jpeg",
+      // });
+      // await newRoom.save();
 
-      req.files.forEach(async (item) => {
-        let compressedImageFileSavePath = await path.join(
-          __dirname,
-          "images",
-          item.filename.split(".", 1) + ".jpeg"
-        );
+      // req.files.forEach((item) => {
+      //   let compressedImageFileSavePath = path.join(
+      //     __dirname,
+      //     "../images",
+      //     item.filename
+      //   );
 
-        sharp(item.path)
-          .jpeg({
-            quality: 80,
-            chromaSubsampling: "4:4:4",
-          })
-          .toFile(compressedImageFileSavePath);
+      //   sharp(item.path)
+      //     .jpeg({
+      //       quality: 80,
+      //       chromaSubsampling: "4:4:4",
+      //     })
+      //     .toFile(compressedImageFileSavePath);
 
-        const newImage = await new Image({
-          idroom: newRoom.id,
-          filename: item.filename.split(".", 1) + ".jpeg",
-          type: 0,
-        });
-        newImage.save();
-      });
+      //   const newImage = new Image({
+      //     idroom: newRoom.id,
+      //     filename: item.filename.split(".", 1) + ".jpeg",
+      //     type: 0,
+      //   });
+      //   newImage.save();
+      // });
 
-      const date = new Date();
-      const minute = 1000 * 60;
-      const hour = minute * 60;
-      const day = hour * 24;
-      const implementationDate = date.getTime() + day * 2;
+      // const date = new Date();
+      // const minute = 1000 * 60;
+      // const hour = minute * 60;
+      // const day = hour * 24;
+      // const implementationDate = date.getTime() + day * 2;
 
-      const newWorkAssignment = new WorkAssignment({
-        idroom: newRoom.id,
-        idaccount: sortarr[0],
-        work: "Duyệt tin",
-        implementationdate: implementationDate,
-        static: 1,
-      });
-      await newWorkAssignment.save();
+      // const newWorkAssignment = new WorkAssignment({
+      //   idroom: newRoom.id,
+      //   idaccount: sortarr[0].idaccount,
+      //   work: "Duyệt tin",
+      //   implementationdate: implementationDate,
+      //   static: 1,
+      // });
+      // newWorkAssignment.save();
 
-      req.body.workinghours.forEach((ele) => {
-        const newWorkinghours = new WorkingHours({
-          idtimeslot: ele.idtimeslot,
-          idroom: newRoom.id,
-          date: ele.date,
-          static: 0,
-        });
-        newWorkinghours.save();
-      });
+      // req.body.workinghours.forEach((ele) => {
+      //   const newWorkinghours = new WorkingHours({
+      //     idtimeslot: ele.idtimeslot,
+      //     idroom: newRoom.id,
+      //     date: ele.date,
+      //     static: 0,
+      //   });
+      //   newWorkinghours.save();
+      // });
 
       res.status(200).json("Add successfully");
-      // res.status(200).json(req.files[0].filename.split(".", 1) + ".jpeg");
     } catch (error) {
       res.status(500).json(error);
     }
